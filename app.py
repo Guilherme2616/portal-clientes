e1c9f06436a42d9635869b5ac5208be854d79aad
Guilherme <guilhermepaularamos@gmail.com>
2026-02-18 20:19:23 -0300

    Teste database url

--- app.py
+++ app.py
@@ -25,13 +25,10 @@ app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_USERNAME")
 mail = Mail(app)
 
 def get_db_connection():
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        port=os.getenv("DB_PORT")
-    )
+    print("DATABASE_URL:", os.getenv("DATABASE_URL"))
+    return psycopg2.connect(os.getenv("DATABASE_URL"))
+
+
 
 @app.route("/")
 def home():
